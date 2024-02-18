e7ecb41bbbd3582c0067560833541f6cbed3e077
RanaZaeem0 <zainibhai333333@gmail.com>
2024-02-18 13:33:19 +0500

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+// const box2items = document.querySelectorAll(".box-2-items");
+
+
+// function bounce() {
+//     box2items.forEach((e) => {
+//         e.addEventListener("mouseover", (event) => {
+
+//             event.classList.add('a')
+//         })
+//     })
+// }
+// bounce()
